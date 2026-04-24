15e179c289afb895d04e02e1097cb20ec1cb0df4
Gilang Setia <gilangbenzema9@gmail.com>
2026-04-24 16:21:45 +0700

    mengganti icon leaderboard di sisi member

--- app/Views/layouts/member_layout.php
+++ app/Views/layouts/member_layout.php
@@ -82,7 +82,7 @@
 
     <a href="<?= base_url('member/leaderboard') ?>"
        class="sidebar-link <?= ($activeNav ?? '') === 'leaderboard' ? 'aktif' : '' ?>">
-      <svg viewBox="0 0 24 24"><line x1="18" y1="20" x2="18" y2="10"/><line x1="12" y1="20" x2="12" y2="4"/><line x1="6" y1="20" x2="6" y2="14"/></svg>
+      <i class="ti ti-trophy"></i>
       Leaderboard
     </a>
   </nav>
